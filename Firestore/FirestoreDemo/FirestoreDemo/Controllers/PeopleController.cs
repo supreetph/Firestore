979f565cafcdb51ba4cdffa1b2fa85a90fd58ddc
Supreet <suprith.prakash.sp@gmail.com>
2020-06-23 13:23:33 +0530

    Added MVC controllers and models

---
+++ Firestore/FirestoreDemo/FirestoreDemo/Controllers/PeopleController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using FirestoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirestoreDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "RuACwgmFjFnMGMGvLPNbqAwhJkZRmI9DTZqrkaE7",
+            BasePath = "https://hip-voyager-241415.firebaseio.com"
+            
+
+        };
+        IFirebaseClient client;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(People people)
+        {
+            InsertData(people);
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+
+        private void InsertData(People people)
+        {
+            client = new FirebaseClient(config);
+            _ = client.Push("People/", people);
+        }
+    }
+}
---
+++ Firestore/FirestoreDemo/FirestoreDemo/Models/People.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirestoreDemo.Models
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public int Age { get; set; }
+    }
+}
